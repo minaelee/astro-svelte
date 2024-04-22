7c6359234fc2a8dba301d6ad3d7ce92c8ebfadd2
minaelee <minae.lee@gmail.com>
2024-04-21 17:29:31 -0700

    update index

---
+++ src/components/Intro.svelte
@@ -0,0 +1,41 @@
+<!-- <script lang="ts">
+	let count = 0;
+
+	function add() {
+		count += 1;
+	}
+
+	function subtract() {
+		count -= 1;
+	}
+</script> -->
+
+<!-- <div class="counter">
+	<button on:click={subtract}>-</button>
+	<pre>{count}</pre>
+	<button on:click={add}>+</button>
+</div> -->
+<div class="message mx-10">
+	<slot />
+</div>
+
+<style>
+	/* .counter {
+		display: grid;
+		font-size: 2em;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		margin-top: 2em;
+		place-items: center;
+	} */
+
+	.message {
+		text-align: center;
+		font-family: 'SourceSansPro', sans-serif;
+		margin-bottom: 2rem;
+
+		/* display: block;
+		margin-left: auto;
+		margin-right: auto; */
+	}
+
+</style>
---
+++ src/components/TechsUsed.svelte
@@ -0,0 +1,20 @@
+
+<div class="message mx-10">
+	<slot />
+</div>
+
+<style>
+	/* .counter {
+		display: grid;
+		font-size: 2em;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		margin-top: 2em;
+		place-items: center;
+	} */
+
+	.message {
+		text-align: center;
+		font-family: 'SourceSansPro', sans-serif;
+	}
+
+</style>
